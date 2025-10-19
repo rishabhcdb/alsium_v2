c0c5e4a5fb4bf79bf9eb82d2c219fde75477d713
Rishabh Singh <123507259+rishabhcdb@users.noreply.github.com>
2025-10-20 00:55:45 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,13 @@
+# index.py
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+
+# Home route
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
